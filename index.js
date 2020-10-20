ea7500218c657cf11d07b729fe2be52b8ab83f7f
hacker19374 <58150407+hacker19374@users.noreply.github.com>
2020-10-20 11:31:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 var x = 1
+var timer = setInterval(changeTitle, 1000);
 var z = "hacker19374's Website"
 function changeTitle() {
-  if (x == 1) {
+  if (x == 0) {
       z = "Home Page";
-  };
+  }
   else {
       z = "hacker19374's Website";
-  };
+  }
   document.title = z;
   x = Math.abs(x - 1);
 
 };
-
-var timer = setInterval(changeTitle, 1000);
