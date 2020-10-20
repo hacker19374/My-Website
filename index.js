e2fee8a4a825aa16569c02048de9204a4b01640f
hacker19374 <58150407+hacker19374@users.noreply.github.com>
2020-10-20 10:59:10 -0400

    Update index.js
    Added a loop to change the title

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function changeTitle(value) {
-  document.title = value;
-}
+var x = 1
+var z = "hacker19374's Website"
+function changeTitle() {
+  if (x == 1) {
+      z = "Home Page";
+  };
+  else {
+      z = "hacker19374's Website";
+  };
+  document.title = z;
+  x = Math.abs(x - 1);
+
+};
+
+var timer = setInterval(changeTitle(z), 1000);
