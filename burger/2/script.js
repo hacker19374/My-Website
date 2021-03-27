7fb795bd18275eba030ae08f0e7cdf7ff7d3d278
hacker19374 <58150407+hacker19374@users.noreply.github.com>
2021-03-27 11:14:58 -0400

    v0.1.4
    4 Changes
    
    1st Playable version, not the official release yet

--- burger/2/script.js
+++ burger/2/script.js
@@ -1,5 +1,15 @@
 var money = 0;
+var clickValue = 1;
 var cursorPrice = 30;
 var grandpaPrice = 200;
 var farmPrice = 2200;
-var minePrice = 24000;
+var cavePrice = 24000;
+
+function update() {
+  document.getElementById("moneyVisible").innerHTML = money;
+}
+
+function clickBurger() {
+  money += clickValue;
+  update();
+}
