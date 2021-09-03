4dbd384c9ffac8ed876c05772fa55e732ac9d384
hacker19374 <58150407+hacker19374@users.noreply.github.com>
2021-09-03 07:57:04 -0400

    Update script.js

--- stocks/script.js
+++ stocks/script.js
@@ -2,8 +2,9 @@ var money = 10;
 var percent = 0.01;
 
 function onCl() {
+ money = money + (money * percent);
  update() 
 }
 function update() {
-  
+  document.getElementById("money").innerHTML = "$" + money. toFixed(2);
 }
