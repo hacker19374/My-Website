65e4e1b798ddefb02d47a32d06a246abb10a03be
hacker19374 <58150407+hacker19374@users.noreply.github.com>
2021-09-03 08:47:58 -0400

    Update script.js

--- stocks/script.js
+++ stocks/script.js
@@ -1,10 +1,22 @@
 var money = 10;
 var percent = 0.01;
+var percPrice = 50;
+
 
 function onCl() {
  money = money + (money * percent);
  update() 
 }
 function update() {
-  document.getElementById("money").innerHTML = "$" + money. toFixed(2);
+  document.getElementById("money").innerHTML = "$" + money.toFixed(2);
+}
+function buyUpPercent() {
+  if (money >= percPrice) { 
+    money -= percPrice;
+			 percPrice += (percPrice * (1/3)).toFixed(0)
+			 percent += 0.1;
+  }
+}
+function autoStock() {
+	
 }
