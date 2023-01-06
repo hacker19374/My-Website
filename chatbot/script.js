6a011351592fcfdac1a6b3eca75d16dc6f042619
hacker19374 <58150407+hacker19374@users.noreply.github.com>
2023-01-06 10:25:39 -0500

    Update script.js

--- chatbot/script.js
+++ chatbot/script.js
@@ -1,12 +1,15 @@
 function sendInput() {
   var input = document.getElementById("userInput").value;
+  var found = false;
   for (var i = 0; i < text.inputs.length; i++) {
     if (input.toLowerCase() == text.inputs[i]) {
       document.getElementById("chatbotResponse").innerHTML = text.outputs[i];
-      
+      found = true;
     }
     else {
+      if (found === false) {
       document.getElementById("chatbotResponse").innerHTML = "Sorry, I don't know how to respond to that, please fix your spelling, or type something I can understand.";
+      }
     }
   }
 }
