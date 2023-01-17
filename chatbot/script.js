c5daec12de363a7a3b1c45d058fc32280da1e14a
hacker19374 <58150407+hacker19374@users.noreply.github.com>
2023-01-17 16:02:15 -0500

    Add files via upload

--- chatbot/script.js
+++ chatbot/script.js
@@ -8,11 +8,59 @@ function sendInput() {
     }
     else {
       if (found === false) {
-      document.getElementById("chatbotResponse").innerHTML = "Sorry, I don't know how to respond to that, please fix your spelling, or type something I can understand.";
+        if ((input.charAt(0) / 1) == isNaN) {
+        document.getElementById("chatbotResponse").innerHTML = "Sorry, I don't know how to respond to that, please fix your spelling, or type something I can understand.";
+        }
+      
+      else {
+        equation();
+      }
       }
     }
   }
 }
+
+
+const form = document.getElementById('userInput');
+const errorElement = document.getElementById('chatbotResponse');
+const resultElement = document.getElementById('chatbotResponse');
+
+function equation() {
+
+  const form = document.getElementById('userInput');
+  const errorElement = document.getElementById('chatbotResponse');
+  const resultElement = document.getElementById('chatbotResponse');
+  
+  const input = document.getElementById('userInput').value;
+  const match = input.match(/(\d+)([+*\/-])(\d+)/);
+  if (!match) {
+    errorElement.textContent = "I can't solve your equation, it might be because there letters in it, fix this error and try again.";
+    return;
+  }
+
+  const [, num1, operation, num2] = match;
+  const n1 = parseInt(num1);
+  const n2 = parseInt(num2);
+
+  let result;
+  switch (operation) {
+    case '+':
+      result = n1 + n2;
+      break;
+    case '-':
+      result = n1 - n2;
+      break;
+    case '*':
+      result = n1 * n2;
+      break;
+    case '/':
+      result = n1 / n2;
+      break;
+  }
+
+  resultElement.textContent = "The answer to that equation is " + result + ".";
+}
+
 
 
 var text =
@@ -21,12 +69,23 @@ var text =
   [
     "hi",
     "hello",
-    "how are you"
+    "how are you",
+    "help"
   ],
 "outputs":
   [
     "Hello! How can I help you today?",
     "Hi there! What can I do for you?",
-    "I'm just a computer program, so I don't have feelings. But I'm here to assist you!"
+    "I'm just a computer program, so I don't have feelings. But I'm here to assist you!",
+    "So far, the things I respond to is 'hi', 'hello', 'help' and 'how are you'. I can also solve 2 number equations, for example, '2/6' or '53+842'"
   ]
 };
+
+
+function load() {
+  document.querySelector('#userInput').addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+      sendInput();
+    }
+  });
+}
